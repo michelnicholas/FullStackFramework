c3f39ea75beee2231c5ce46ef35ca74a0e3bed47
Michel Nicholas <michelnicholas1993@gmail.com>
2022-02-01 20:29:50 -0500

    created one json file for one object

--- JsonJava/src/main/java/JsonToJava.java
+++ JsonJava/src/main/java/JsonToJava.java
@@ -1,11 +1,17 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 
 public class JsonToJava {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         // My Sql Connection to Database
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn = null;
+        // Giving Details to CustomerDetails class
+        CustomerDetails c = new CustomerDetails();
         conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business","root","Imani11234!");
 
         // Object of Statement Class will help us to execute queries
@@ -22,8 +28,7 @@ public class JsonToJava {
 //       rs.getInt(3);
 //       rs.getString(4);
         while (rs.next()){
-            // Giving Details to CustomerDetails class
-            CustomerDetails c = new CustomerDetails();
+
 
             // Used set methods to put the information into Customer Details methods
             c.setCourseName(rs.getString(1));
@@ -34,8 +39,13 @@ public class JsonToJava {
             // Print the information from the Sql Library to console by using Getter Methods
             System.out.println(c.getCourseName());
             System.out.println(c.getPurchasedDate());
-//
+            ObjectMapper oj = new ObjectMapper();
+            oj.writeValue(new File("C:\\Users\\Miche\\IdeaProjects\\FullStackAutomationProject\\JsonJava\\customerInfo.json"),c);
+
         }
+
+
+
         conn.close();
     }
 
