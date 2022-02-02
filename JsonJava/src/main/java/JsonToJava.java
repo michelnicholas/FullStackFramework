38986a49fe7f48a43f73f749846bfda32220892f
Michel Nicholas <michelnicholas1993@gmail.com>
2022-02-02 08:40:16 -0500

    added muliple json files from sql query

--- JsonJava/src/main/java/JsonToJava.java
+++ JsonJava/src/main/java/JsonToJava.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
@@ -7,19 +9,19 @@ import java.util.ArrayList;
 
 public class JsonToJava {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException, JsonGenerationException, JsonMappingException {
         // My Sql Connection to Database
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn = null;
         // Giving Details to CustomerDetails class
-
         ArrayList<CustomerDetails> a = new ArrayList<CustomerDetails>();
+
         // Giving connection to the Driver
         conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Business","root","Imani11234!");
 
         // Object of Statement Class will help us to execute queries
         Statement st = conn.createStatement();
-        ResultSet rs =  st.executeQuery("select * from CustomerInfo where location = 'Asia' and purchasedDate=curdate() LIMIT 1;");
+        ResultSet rs =  st.executeQuery("select * from CustomerInfo where location = 'Asia' and purchasedDate=curdate();");
 //       rs.next();
 //       rs.getString(1);
 //       rs.getString(2);
@@ -31,27 +33,21 @@ public class JsonToJava {
 //       rs.getInt(3);
 //       rs.getString(4);
         while (rs.next()){
-
-
-            CustomerDetails c = new CustomerDetails();
+            CustomerDetails c=new CustomerDetails();
             // 3 different json files , 3 different java objects
             // Used set methods to put the information into Customer Details methods
             c.setCourseName(rs.getString(1));
             c.setPurchasedDate(rs.getString(2));
             c.setAmount(rs.getInt(3));
             c.setLocation(rs.getString(4));
+            // Adding java objects from sql query
             a.add(c);
 
-
-            // Print the information from the Sql Library to console by using Getter Methods
-            System.out.println(c.getCourseName());
-            System.out.println(c.getPurchasedDate());
+            }
+        for (int i = 0; i <a.size();i++){
             ObjectMapper oj = new ObjectMapper();
-            oj.writeValue(new File("C:\\Users\\Miche\\IdeaProjects\\FullStackAutomationProject\\JsonJava\\customerInfo.json"),c);
-
+            oj.writeValue(new File("C:\\Users\\Miche\\IdeaProjects\\FullStackAutomationProject\\JsonJava\\customerInfo" + i+ ".json"),a.get(i));
         }
-
-
 
         conn.close();
     }
